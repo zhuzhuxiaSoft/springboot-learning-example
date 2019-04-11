c5caa83c485f6d1147b23890e08de67e53503ee9
zzx <1085670157@qq.com>
2019-04-12 02:01:48 +0800

    调整项目

--- chapter-4-spring-boot-validating-form-input/src/main/java/spring/boot/core/service/impl/UserServiceImpl.java
+++ chapter-4-spring-boot-validating-form-input/src/main/java/spring/boot/core/service/impl/UserServiceImpl.java
@@ -4,11 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import spring.boot.core.domain.User;
 import spring.boot.core.domain.UserRepository;
 import spring.boot.core.service.UserService;
 
 import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
 
 /**
  * User 业务层实现
@@ -22,25 +25,24 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+    
+    @Autowired
+    EntityManagerFactory entityManagerFactory;
 
-    @Override
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    @Override
     public User insertByUser(User user) {
         LOGGER.info("新增用户：" + user.toString());
         return userRepository.save(user);
     }
 
-    @Override
     public User update(User user) {
         LOGGER.info("更新用户：" + user.toString());
         return userRepository.save(user);
     }
 
-    @Override
     public User delete(Long id) {
         User user = userRepository.findById(id).get();
         userRepository.delete(user);
@@ -49,7 +51,6 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
-    @Override
     public User findById(Long id) {
         LOGGER.info("获取用户 ID ：" + id);
         return userRepository.findById(id).get();
--- springboot-properties/src/test/java/org/spring/springboot/property/PropertiesTest.java
+++ springboot-properties/src/test/java/org/spring/springboot/property/PropertiesTest.java
@@ -1,6 +1,5 @@
 package org.spring.springboot.property;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
